9216529adf5d4eb0193af918b422573d4b8f3d63
LeePaulison <lee.paulison.jr@gmail.com>
2025-07-05 11:50:02 -0400

    Added Susspence to Signin page

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,43 +1,18 @@
 'use client';
-// Next JS imports
-import Link from 'next/link';
-// Next Auth imports
-import { signIn } from 'next-auth/react';
+// React imports
+import { Suspense } from 'react';
 // Next Router imports
 import { useSearchParams } from 'next/navigation';
-// Shadcn UI imports
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
+// Component imports
+import SignInInner from '@/components/signInInner';
 
 export default function SignInPage() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/';
 
   return (
-    <main className='flex items-center justify-center w-full h-full'>
-      <Card className='card p-8 rounded-xl max-w-sm w-full shadow-lg my-auto'>
-        <h1 className='text-3xl font-bold mb-6 mx-auto'>Sign In</h1>
-        <p className='text-muted-foreground mb-6 text-center'>Sign in to your account to continue.</p>
-
-        <div className='flex flex-row items-center space-x-4 ms-auto'>
-          <Link href={callbackUrl}>
-            <Button
-              variant='outline'
-              asChild
-              className='border-muted text-accent hover:bg-accent hover:text-accent-foreground'
-            >
-              <span>Cancel</span>
-            </Button>
-          </Link>
-          <Button
-            variant='outline'
-            className='border-muted text-accent hover:bg-accent hover:text-accent-foreground'
-            onClick={() => signIn('github', { callbackUrl })}
-          >
-            GitHub
-          </Button>
-        </div>
-      </Card>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignInInner callbackUrl={callbackUrl} />
+    </Suspense>
   );
 }
---
+++ components/signInInner.jsx
@@ -0,0 +1,39 @@
+'use client';
+// Next JS imports
+import Link from 'next/link';
+// Next Auth imports
+import { signIn } from 'next-auth/react';
+// Shadcn UI imports
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+
+export default function SignInInner({ callbackUrl }) {
+
+  return (
+    <main className='flex items-center justify-center w-full h-full'>
+      <Card className='card p-8 rounded-xl max-w-sm w-full shadow-lg my-auto'>
+        <h1 className='text-3xl font-bold mb-6 mx-auto'>Sign In</h1>
+        <p className='text-muted-foreground mb-6 text-center'>Sign in to your account to continue.</p>
+
+        <div className='flex flex-row items-center space-x-4 ms-auto'>
+          <Link href={callbackUrl}>
+            <Button
+              variant='outline'
+              asChild
+              className='border-muted text-accent hover:bg-accent hover:text-accent-foreground'
+            >
+              <span>Cancel</span>
+            </Button>
+          </Link>
+          <Button
+            variant='outline'
+            className='border-muted text-accent hover:bg-accent hover:text-accent-foreground'
+            onClick={() => signIn('github', { callbackUrl })}
+          >
+            GitHub
+          </Button>
+        </div>
+      </Card>
+    </main>
+  );
+}
