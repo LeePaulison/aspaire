e4cd9d342d14d6b345a7eb4c722efd82eebf7842
LeePaulison <lee.paulison.jr@gmail.com>
2025-07-05 11:16:17 -0400

    Pallette Accessibility correction

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,4 +1,6 @@
 'use client';
+// Next JS imports
+import Link from 'next/link';
 // Next Auth imports
 import { signIn } from 'next-auth/react';
 // Next Router imports
@@ -15,14 +17,26 @@ export default function SignInPage() {
     <main className='flex items-center justify-center w-full h-full'>
       <Card className='card p-8 rounded-xl max-w-sm w-full shadow-lg my-auto'>
         <h1 className='text-3xl font-bold mb-6 mx-auto'>Sign In</h1>
+        <p className='text-muted-foreground mb-6 text-center'>Sign in to your account to continue.</p>
 
-        <Button
-          variant='outline'
-          className='border-muted text-accent hover:bg-accent hover:text-accent-foreground w-full'
-          onClick={() => signIn('github', { callbackUrl })}
-        >
-          Sign in with GitHub
-        </Button>
+        <div className='flex flex-row items-center space-x-4 ms-auto'>
+          <Link href={callbackUrl}>
+            <Button
+              variant='outline'
+              asChild
+              className='border-muted text-accent hover:bg-accent hover:text-accent-foreground'
+            >
+              <span>Cancel</span>
+            </Button>
+          </Link>
+          <Button
+            variant='outline'
+            className='border-muted text-accent hover:bg-accent hover:text-accent-foreground'
+            onClick={() => signIn('github', { callbackUrl })}
+          >
+            GitHub
+          </Button>
+        </div>
       </Card>
     </main>
   );
--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,6 +1,9 @@
 'use client';
 // Next-Auth imports
 import { useSession, signIn, signOut } from 'next-auth/react';
+// Next JS Components
+import Image from 'next/image';
+import Link from 'next/link';
 // Shadcn UI components
 import { Button } from '@/components/ui/button'
 // Theme toggle component
@@ -11,7 +14,9 @@ export function Header() {
 
   return (
     <header className='container flex flex-row items-center justify-between w-full py-2 px-4 border-b border-b-muted'>
-      <h1 className='text-xl font-bold'>AspAIre</h1>
+      <Link href='/' className='flex items-center space-x-2'>
+        <Image src='/assets/icons/favicon.svg' alt="Lee Paulison's Logo" width={40} height={40} priority className='rounded-full' />
+      </Link>
       <div className='flex items-center space-x-4'>
         <ThemeToggle />
 
